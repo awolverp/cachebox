e3fb21cddfc645519351527cb7fe43cda6c186eb
awolverp <awolverp@gmail.com>
2025-04-04 16:07:03 +0330

    - Rewrite the Cache class in Python - Optimize the `isize` to `u64` strategy - Now we don't ignore the errors white doing equal operations

---
+++ src/bridge/cache.rs
@@ -0,0 +1,319 @@
+//! implement Cache, a simple cache without any algorithms and policies
+
+use crate::common::{Entry, ObservedIterator, PreHashObject};
+
+/// A simple cache that has no algorithm; this is only a hashmap.
+///
+/// `Cache` vs `dict`:
+/// - it is thread-safe and unordered, while `dict` isn't thread-safe and ordered (Python 3.6+).
+/// - it uses very lower memory than `dict`.
+/// - it supports useful and new methods for managing memory, while `dict` does not.
+/// - it does not support `popitem`, while `dict` does.
+/// - You can limit the size of [`Cache`], but you cannot for `dict`.
+#[pyo3::pyclass(module = "cachebox._core", frozen)]
+pub struct Cache {
+    raw: parking_lot::Mutex<crate::policies::nopolicy::NoPolicy>,
+}
+
+#[pyo3::pymethods]
+impl Cache {
+    #[new]
+    #[pyo3(signature=(maxsize, *, capacity=0))]
+    fn __new__(maxsize: usize, capacity: usize) -> pyo3::PyResult<Self> {
+        let raw = crate::policies::nopolicy::NoPolicy::new(maxsize, capacity)?;
+
+        let self_ = Self {
+            raw: parking_lot::Mutex::new(raw),
+        };
+        Ok(self_)
+    }
+
+    fn _state(&self) -> usize {
+        self.raw.lock().observed.get() as usize
+    }
+
+    fn maxsize(&self) -> usize {
+        self.raw.lock().maxsize()
+    }
+
+    fn capacity(&self) -> usize {
+        self.raw.lock().capacity()
+    }
+
+    fn __len__(&self) -> usize {
+        self.raw.lock().len()
+    }
+
+    fn __sizeof__(&self) -> usize {
+        let lock = self.raw.lock();
+        lock.capacity()
+            * (std::mem::size_of::<PreHashObject>() + std::mem::size_of::<pyo3::ffi::PyObject>())
+    }
+
+    fn __contains__(&self, py: pyo3::Python<'_>, key: pyo3::PyObject) -> pyo3::PyResult<bool> {
+        let key = PreHashObject::from_pyobject(py, key)?;
+        let lock = self.raw.lock();
+
+        match lock.lookup(py, &key)? {
+            Some(_) => Ok(true),
+            None => Ok(false),
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.raw.lock().is_empty()
+    }
+
+    fn is_full(&self) -> bool {
+        self.raw.lock().is_full()
+    }
+
+    fn insert(
+        &self,
+        py: pyo3::Python<'_>,
+        key: pyo3::PyObject,
+        value: pyo3::PyObject,
+    ) -> pyo3::PyResult<Option<pyo3::PyObject>> {
+        let key = PreHashObject::from_pyobject(py, key)?;
+        let mut lock = self.raw.lock();
+
+        match lock.entry(py, &key)? {
+            Entry::Occupied(mut entry) => Ok(Some(entry.update(value)?)),
+            Entry::Absent(entry) => {
+                entry.insert(key, value)?;
+                Ok(None)
+            }
+        }
+    }
+
+    fn get(
+        &self,
+        py: pyo3::Python<'_>,
+        key: pyo3::PyObject,
+        default: pyo3::PyObject,
+    ) -> pyo3::PyResult<pyo3::PyObject> {
+        let key = PreHashObject::from_pyobject(py, key)?;
+        let lock = self.raw.lock();
+
+        match lock.lookup(py, &key)? {
+            Some(val) => Ok(val.clone_ref(py)),
+            None => Ok(default),
+        }
+    }
+
+    fn remove(&self, py: pyo3::Python<'_>, key: pyo3::PyObject) -> pyo3::PyResult<()> {
+        let key = PreHashObject::from_pyobject(py, key)?;
+        let mut lock = self.raw.lock();
+
+        match lock.entry(py, &key)? {
+            Entry::Occupied(entry) => {
+                entry.remove();
+                Ok(())
+            }
+            Entry::Absent(_) => Err(pyo3::PyErr::new::<pyo3::exceptions::PyKeyError, _>(key.obj)),
+        }
+    }
+
+    fn update(
+        slf: pyo3::PyRef<'_, Self>,
+        py: pyo3::Python<'_>,
+        iterable: pyo3::PyObject,
+    ) -> pyo3::PyResult<()> {
+        if slf.as_ptr() == iterable.as_ptr() {
+            return Ok(());
+        }
+
+        let mut lock = slf.raw.lock();
+        lock.extend(py, iterable)
+    }
+
+    fn __richcmp__(
+        slf: pyo3::PyRef<'_, Self>,
+        other: pyo3::PyObject,
+        op: pyo3::class::basic::CompareOp,
+    ) -> pyo3::PyResult<bool> {
+        let other = match other.extract::<pyo3::PyRef<'_, Self>>(slf.py()) {
+            Ok(o) => o,
+            Err(_) => return Ok(false),
+        };
+
+        match op {
+            pyo3::class::basic::CompareOp::Eq => {
+                if slf.as_ptr() == other.as_ptr() {
+                    return Ok(true);
+                }
+
+                let t1 = slf.raw.lock();
+                let t2 = other.raw.lock();
+                t1.equal(slf.py(), &*t2)
+            }
+            pyo3::class::basic::CompareOp::Ne => {
+                if slf.as_ptr() != other.as_ptr() {
+                    return Ok(true);
+                }
+
+                let t1 = slf.raw.lock();
+                let t2 = other.raw.lock();
+                t1.equal(slf.py(), &*t2).map(|r| !r)
+            }
+            _ => Err(pyo3::PyErr::new::<pyo3::exceptions::PyTypeError, _>(
+                "only '==' or '!=' are supported",
+            )),
+        }
+    }
+
+    fn pop(
+        &self,
+        py: pyo3::Python<'_>,
+        key: pyo3::PyObject,
+        default: pyo3::PyObject,
+    ) -> pyo3::PyResult<pyo3::PyObject> {
+        let key = PreHashObject::from_pyobject(py, key)?;
+        let mut lock = self.raw.lock();
+
+        match lock.entry(py, &key)? {
+            Entry::Occupied(entry) => {
+                let (_, value) = entry.remove();
+                Ok(value)
+            }
+            Entry::Absent(_) => Ok(default),
+        }
+    }
+
+    fn clear(&self, reuse: bool) {
+        let mut lock = self.raw.lock();
+        lock.clear();
+
+        if !reuse {
+            lock.shrink_to_fit();
+        }
+    }
+
+    fn shrink_to_fit(&self) {
+        let mut lock = self.raw.lock();
+        lock.shrink_to_fit();
+    }
+
+    fn setdefault(
+        &self,
+        py: pyo3::Python<'_>,
+        key: pyo3::PyObject,
+        default: pyo3::PyObject,
+    ) -> pyo3::PyResult<pyo3::PyObject> {
+        let key = PreHashObject::from_pyobject(py, key)?;
+        let mut lock = self.raw.lock();
+
+        match lock.entry(py, &key)? {
+            Entry::Occupied(entry) => {
+                let (_, ref value) = entry.into_value();
+                Ok(value.clone_ref(py))
+            }
+            Entry::Absent(entry) => {
+                entry.insert(key, default.clone_ref(py))?;
+                Ok(default)
+            }
+        }
+    }
+
+    fn items(slf: pyo3::PyRef<'_, Self>) -> pyo3::PyResult<pyo3::Py<cache_items>> {
+        let lock = slf.raw.lock();
+        let state = lock.observed.get();
+        let iter = lock.iter();
+
+        let result = cache_items {
+            ptr: ObservedIterator::new(slf.as_ptr(), state),
+            iter: parking_lot::Mutex::new(iter),
+        };
+
+        pyo3::Py::new(slf.py(), result)
+    }
+
+    fn __getnewargs__(&self) -> (usize,) {
+        (0,)
+    }
+
+    fn __getstate__(&self, py: pyo3::Python<'_>) -> pyo3::PyResult<pyo3::PyObject> {
+        let lock = self.raw.lock();
+        unsafe {
+            let state = {
+                let mp = pyo3::ffi::PyDict_New();
+
+                if mp.is_null() {
+                    return Err(pyo3::PyErr::fetch(py));
+                }
+
+                for bucket in lock.iter() {
+                    let (key, val) = bucket.as_ref();
+                    // SAFETY: we don't need to check error because we sure about key that is hashable.
+                    pyo3::ffi::PyDict_SetItem(mp, key.obj.as_ptr(), val.as_ptr());
+                }
+
+                let maxsize = pyo3::ffi::PyLong_FromSize_t(lock.maxsize());
+                let capacity = pyo3::ffi::PyLong_FromSize_t(lock.capacity());
+
+                #[allow(unused_unsafe)]
+                tuple!(
+                    py,
+                    3,
+                    0 => maxsize,
+                    1 => mp,
+                    2 => capacity,
+                )?
+            };
+            Ok(pyo3::Py::from_owned_ptr(py, state))
+        }
+    }
+
+    pub fn __setstate__(&self, py: pyo3::Python<'_>, state: pyo3::PyObject) -> pyo3::PyResult<()> {
+        let mut lock = self.raw.lock();
+        lock.from_pickle(py, state.as_ptr())
+    }
+
+    pub fn __traverse__(&self, visit: pyo3::PyVisit<'_>) -> Result<(), pyo3::PyTraverseError> {
+        for value in self.raw.lock().iter() {
+            let (key, value) = unsafe { value.as_ref() };
+            visit.call(&key.obj)?;
+            visit.call(value)?;
+        }
+        Ok(())
+    }
+
+    pub fn __clear__(&self) {
+        let mut lock = self.raw.lock();
+        lock.clear()
+    }
+}
+
+#[allow(non_camel_case_types)]
+#[pyo3::pyclass(module = "cachebox._core")]
+pub struct cache_items {
+    pub ptr: ObservedIterator,
+    pub iter: parking_lot::Mutex<hashbrown::raw::RawIter<(PreHashObject, pyo3::PyObject)>>,
+}
+
+#[pyo3::pymethods]
+impl cache_items {
+    fn __iter__(slf: pyo3::PyRef<'_, Self>) -> pyo3::PyRef<'_, Self> {
+        slf
+    }
+
+    #[allow(unused_mut)]
+    fn __next__(mut slf: pyo3::PyRefMut<'_, Self>) -> pyo3::PyResult<*mut pyo3::ffi::PyObject> {
+        let mut iter = slf.iter.lock();
+
+        slf.ptr.proceed(slf.py())?;
+
+        if let Some(x) = iter.next() {
+            let (key, val) = unsafe { x.as_ref() };
+
+            tuple!(
+                slf.py(),
+                2,
+                0 => key.obj.clone_ref(slf.py()).into_ptr(),
+                1 => val.clone_ref(slf.py()).into_ptr(),
+            )
+        } else {
+            Err(pyo3::PyErr::new::<pyo3::exceptions::PyStopIteration, _>(()))
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 use pyo3::prelude::*;
 
+#[macro_use]
+mod common;
+
+mod bridge;
+mod policies;
+
 /// cachebox core ( written in Rust )
 #[pymodule(gil_used = false)]
 #[cold]
 fn _core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__author__", env!("CARGO_PKG_AUTHORS"))?;
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    
+
+    m.add_class::<bridge::cache::Cache>()?;
+
     Ok(())
 }

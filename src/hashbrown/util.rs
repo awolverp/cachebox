cbac54266a026867f121b134dc97d081149b1f44
awolverp <awolverp@gmail.com>
2026-07-26 22:56:59 +0330

    Remove nightly `unsafe impl Drop <#[may_dangle] T...` implementation for LazyHeap

--- src/hashbrown/util.rs
+++ src/hashbrown/util.rs
@@ -5,12 +5,27 @@ pub(crate) use std::hint::unlikely;
 
 #[cfg(not(feature = "nightly"))]
 #[inline(always)]
+#[cold]
+fn cold_path() {}
+
+#[cfg(not(feature = "nightly"))]
+#[inline(always)]
 pub(crate) fn likely(b: bool) -> bool {
-    b
+    if b {
+        true
+    } else {
+        cold_path();
+        false
+    }
 }
 
 #[cfg(not(feature = "nightly"))]
 #[inline(always)]
 pub(crate) fn unlikely(b: bool) -> bool {
-    b
+    if b {
+        cold_path();
+        true
+    } else {
+        false
+    }
 }
